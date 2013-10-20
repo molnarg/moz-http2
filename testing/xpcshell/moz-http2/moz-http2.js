f70ab8ad841389ad621bdfab65a55fc22f2cb1dc
Gábor Molnár <gabor@molnar.es>
2013-10-20 18:19:56 +0200

    Adding code to statisfy a very simple test case to moz-http2.js

--- testing/xpcshell/moz-http2/moz-http2.js
+++ testing/xpcshell/moz-http2/moz-http2.js
@@ -4,9 +4,31 @@
 
 var http2 = require('../node-http2');
 var fs = require('fs');
+var url = require('url');
+
+function getHttpContent(path) {
+  var content = '<!doctype html>' +
+    '<html>' +
+    '<head><title>HOORAY!</title></head>' +
+    '<body>You Win! (by requesting' + path + ')</body>' +
+    '</html>';
+  return content;
+}
 
 function handleRequest(req, res) {
-  res.end('Hello World!');
+  var u = url.parse(req.url);
+  var content = getHttpContent(u.pathname);
+
+  if (req.stream) {
+    res.setHeader('X-Connection-Http2', 'yes');
+    res.setHeader('X-Spdy-StreamId', '' + req.stream.id);
+  } else {
+    res.setHeader('X-Connection-Http2', 'no');
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.writeHead(200);
+  res.end(content);
 }
 
 // Set up the SSL certs for our server
@@ -16,5 +38,13 @@ var options = {
   ca: fs.readFileSync(__dirname + '/../moz-spdy/spdy-ca.pem')
 };
 
-http2.createServer(options, handleRequest).listen(4444);
+var server = http2.createServer(options, handleRequest);
+server.on('connection', function(socket) {
+  socket.on('error', function() {
+    // Ignoring SSL socket errors, since they usually represent a connection that was tore down
+    // by the browser because of an untrusted certificate. And this happens at least once, when
+    // the first test case if done.
+  });
+});
+server.listen(4444);
 console.log('HTTP2 server listening on port 4444');
